fbda3dec30167507d2e7ea57d6bd74d12c30d0bb
Bruno Miranda <brunomb.ufcg@gmail.com>
2013-08-22 16:43:32 -0300

    Consultas 03 e 04 OK

---
+++ consultas/consulta04.sql
@@ -0,0 +1,3 @@
+﻿/*4) Qual a distância entre os centróides de Campina Grande e Patos? (Resposta convertida em KM)*/
+SELECT ST_Distance(ST_Transform(ST_Centroid(a.geom),29195), ST_Transform(ST_Centroid(b.geom),29195)) / 1000
+FROM sig.municipios a, sig.municipios b WHERE a.gid = 52 AND b.gid = 136
