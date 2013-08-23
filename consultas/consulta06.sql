43debe94e2347701ce5447ae540d6ffdc430fa39
Bruno Miranda <brunomb.ufcg@gmail.com>
2013-08-22 21:16:44 -0300

    Consulta 07

--- consultas/consulta06.sql
+++ consultas/consulta06.sql
@@ -1,5 +1,5 @@
-﻿/*6- Quantos postos pluviométricos estão localizados no município	de Campina Grande? */
-SELECT estacao
+﻿/*6- Quantos postos pluviométricos estão localizados no município de Campina Grande? */
+SELECT posto
 FROM
-(SELECT posto.estacao, ST_Contains((ST_Transform(cidade_a.geom,29195)),(ST_Transform(posto.geom,29195))) AS contains
+(SELECT posto.estacao AS posto, ST_Contains((ST_Transform(cidade_a.geom,29195)),(ST_Transform(posto.geom,29195))) AS contains
 FROM sig.municipios cidade_a, sig.postos posto WHERE cidade_a.gid = 52) AS consulta_postos WHERE consulta_postos.contains = true
