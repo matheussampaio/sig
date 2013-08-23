e5ecf7569107e25d276b58f8d7334e26696e1722
Bruno Miranda <brunomb.ufcg@gmail.com>
2013-08-22 22:19:45 -0300

    Consulta 11

---
+++ consultas/consulta11.sql
@@ -0,0 +1,5 @@
+﻿/*11- Qual ferrovia corta o maior número de municípios?*/
+SELECT consulta_ferrovias.ferrovia, COUNT(consulta_ferrovias.cidade) AS cidades
+FROM
+(SELECT  ferrovia.gid AS ferrovia, cidade_a.nome_munic AS cidade, ST_Crosses((ST_Transform(cidade_a.geom,29195)),(ST_Transform(ferrovia.geom,29195))) AS crosses
+FROM sig.municipios cidade_a, sig.estrada_ferro ferrovia) AS consulta_ferrovias WHERE consulta_ferrovias.crosses = true GROUP BY ferrovia ORDER BY cidades DESC LIMIT 1
