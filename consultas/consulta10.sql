334bc29afd5b53de7cb604a2cb02af90e72e846e
Bruno Miranda <brunomb.ufcg@gmail.com>
2013-08-22 22:05:44 -0300

    Consulta 10

---
+++ consultas/consulta10.sql
@@ -0,0 +1,6 @@
+﻿/* 10- Quais os	postos que estão num raio de 15km da BR-230? */
+
+SELECT posto 
+FROM
+(SELECT posto.estacao AS posto, ST_Contains((ST_Buffer(ST_Transform(rodovia.geom,29195), 15000)),(ST_Transform(posto.geom,29195))) AS contains
+FROM sig.rodovias rodovia, sig.postos posto WHERE rodovia.codrodov = 'BR-230') AS consulta_buffer WHERE consulta_buffer.contains = true GROUP BY posto
